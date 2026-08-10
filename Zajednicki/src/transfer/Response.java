7bd21f5106a25abc97016e1a33756baa8bb1f93e
LukaMarjanov <marjanovluka@gmail.com>
2026-08-11 01:17:39 +0200

    dodati paketi domain, transfer i transfer.util
    U paketu transfer su dodate klase request i response a u transfer.util-u interface Operation i enum ResponseStatus

---
+++ Zajednicki/src/transfer/Response.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package transfer;
+
+import java.io.Serializable;
+import transfer.util.ResponseStatus;
+
+/**
+ *
+ * @author Luka
+ */
+public class Response implements Serializable{
+    
+    private Object data;
+    private Exception exc;
+    private ResponseStatus responseStatus;
+
+    public Response(Object data, Exception exc, ResponseStatus responseStatus) {
+        this.data = data;
+        this.exc = exc;
+        this.responseStatus = responseStatus;
+    }
+
+    public Response() {
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Exception getExc() {
+        return exc;
+    }
+
+    public void setExc(Exception exc) {
+        this.exc = exc;
+    }
+
+    public ResponseStatus getResponseStatus() {
+        return responseStatus;
+    }
+
+    public void setResponseStatus(ResponseStatus responseStatus) {
+        this.responseStatus = responseStatus;
+    }
+}
