66bd68d0119ceeffb5d635478ef017752a82e9ff
LukaMarjanov <marjanovluka@gmail.com>
2026-08-14 02:27:47 +0200

    Niti i forme
    Dodate login i glavna klijentska forma na klijentskoj strani. Na serverskoj kreirane dve niti, za pokretanje servera i za obradu klijentskih zahteva

---
+++ Server/src/thread/ThreadClient.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thread;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import transfer.Request;
+import transfer.Response;
+import transfer.util.ResponseStatus;
+
+/**
+ *
+ * @author Luka
+ */
+public class ThreadClient extends Thread {
+
+    Socket socket;
+
+    public ThreadClient(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        while (!socket.isClosed()) {
+            try {
+                //primamo klijentski zahtev
+                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                Request request = (Request) ois.readObject();
+
+                //Switch case
+                Response response = handleRequest(request);
+
+                //saljemo odgovor klijentu
+                ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                oos.writeObject(response);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
+    }
+
+    private Response handleRequest(Request request) {
+        Response response = new Response(null, null, ResponseStatus.Success);
+        try {
+            switch (request.getOperation()) {
+                
+                
+                default:
+                    return null;
+            }
+        } catch (Exception e) {
+            response.setResponseStatus(ResponseStatus.Error);
+            response.setExc(e);
+        }
+        return response;
+    }
+
+}
